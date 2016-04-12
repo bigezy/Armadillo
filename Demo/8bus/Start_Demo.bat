7e8ac38bd6ba6797948589658c166cf5c8cac1ab
Olivier <osoubigo@illinois.edu>
2016-04-12 10:11:33 -0500

    Tornado update

--- Demo/8bus/Start_Demo.bat
+++ Demo/8bus/Start_Demo.bat
@@ -2,8 +2,8 @@ start C:\armadillo\Demo\8bus\bin\runCyPSA.bat 8bus 10.31.1.201
 
 cd /d "C:\armadillo\Demo\8bus\www"
 
-start C:\armadillo\Demo\8bus\www\start_tornado.exe
+start C:\armadillo\Demo\8bus\www\start_tornado.exe C:\armadillo\Demo\8bus\projects\8bus\npv
 
-TIMEOUT /T 15
+TIMEOUT /T 10
 
 start "" "c:\program files (x86)\google\chrome\application\chrome.exe" --new-window "http://localhost:8888"
