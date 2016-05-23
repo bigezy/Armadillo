6292308607a03ce9a75a67a115816f7c6f0be2e2
Olivier <osoubigo@illinois.edu>
2016-05-23 16:03:23 -0500

    Updated start demo script

--- Demo/8bus/Start_Demo.bat
+++ Demo/8bus/Start_Demo.bat
@@ -1,9 +1,16 @@
-start C:\armadillo\Demo\8bus\bin\runCyPSA.bat 8bus 10.31.1.201
+REM start C:\armadillo\Demo\8bus\bin\runCyPSA.bat 8bus 10.31.1.201
+
+cd /d "C:\armadillo\Demo\8bus\www\model-server"
+
+start C:\python27\python.exe C:\armadillo\Demo\8bus\www\model-server\src\server.py
+
+TIMEOUT /T 2
 
 cd /d "C:\armadillo\Demo\8bus\www"
 
-start C:\armadillo\Demo\8bus\www\start_tornado.exe C:\armadillo\Demo\8bus\projects\8bus\npv
+start C:\armadillo\Demo\8bus\www\start_tornado.exe 
+REM C:\armadillo\Demo\8bus\projects\8bus\npv
 
-TIMEOUT /T 10
+TIMEOUT /T 2
 
 start "" "c:\program files (x86)\google\chrome\application\chrome.exe" --new-window "http://localhost:8888"
